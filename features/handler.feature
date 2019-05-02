e66278340dcf442a55d0189959afd3637004d12f
Martin Kemp <martinkemp@deloitte.co.uk>
2019-05-02 15:58:09 +0100

    More testing changes

--- features/handler.feature
+++ features/handler.feature
@@ -1,9 +1,25 @@
-# Created by martinkemp at 2019-04-24
+# Created by Martin Kemp at 2019-04-24
 Feature: Handler
-  # Enter feature description here
+  The lambda handler
 
-  Scenario: valid handler call
+  Scenario: A valid handler call
     Given a valid payload was sent
+    Then it should set the message
     When I call the handler
     Then it should send the message
-    And return a response
+    And it should return a valid response
+
+  Scenario Outline: Valid message types
+
+    Given a valid "<message_type>" was sent
+    Then it should use the correct "<sender>"
+    Examples: Event
+      | message_type | sender    |
+      | email        | GovNotify |
+      | sms          | GovNotify |
+      | teams        | Teams     |
+
+  Scenario: invalid message type sent
+    Given an invalid message type was sent
+    Then it should log a ValueError Exception
+    And it should raise the Exception
--- features/steps/handler.py
+++ features/steps/handler.py
@@ -1,4 +1,6 @@
 from behave import *
+from notify import Handler
+from hamcrest import assert_that, equal_to, is_in, is_, not_none
 
 use_step_matcher("re")
 
@@ -8,7 +10,25 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    raise NotImplementedError(u'STEP: Given a valid payload was sent')
+    context.handler = Handler({
+        "message_type": "email",
+        "to": "example@example.com",
+        "template_id": "1234-abcd-6789-efff",
+        "template_vars": {
+            "example_var": "var",
+            "name": "Guido"
+        },
+        "attachment": "eyJkYXRhIjogInRoaXMgaXMgYW4gZXhhbXBsZSBiYXNlNjQgZW5jb2RlZCBmaWxlIn0K",
+        "attachment_name": "example.json"
+    })
+
+
+@then("it should set the message")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    assert context.handler.message is not None
 
 
 @when("I call the handler")
@@ -16,7 +36,7 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    raise NotImplementedError(u'STEP: When I call the handler')
+    context.handler.handle()
 
 
 @then("it should send the message")
@@ -24,12 +44,60 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    raise NotImplementedError(u'STEP: Then it should send the message')
+    assert_that(context.handler.data, not_none())
 
 
-@step("return a response")
+@step("it should return a valid response")
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    raise NotImplementedError(u'STEP: And return a response')
+    returned_with_data = {"response": context.handler.data}
+    returned_with_no_response = {
+        "response": f'Success but no response from sender: {type(context.handler.sender).__name__}'}
+    assert_that(context.handler.response, is_in([returned_with_data, returned_with_no_response]))
+
+
+@given('a valid "(?P<message_type>.+)" was sent')
+def step_impl(context, message_type):
+    """
+    :type context: behave.runner.Context
+    :type message_type: str
+    """
+    context.handler = Handler({
+        "message_type": message_type
+    })
+
+
+@then('it should use the correct "(?P<sender>.+)"')
+def step_impl(context, sender):
+    """
+    :type context: behave.runner.Context
+    :type sender: str
+    """
+    assert_that(context.handler.sender.__name__, equal_to(sender))
+
+
+@given("an invalid message type was sent")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    context.handler = Handler({
+        "message_type": "invalid"
+    })
+
+
+@then("it should log a ValueError Exception")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+
+
+@step("it should raise the Exception")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: And it should raise the Exception')
